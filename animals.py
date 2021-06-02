3e576f402626f1d992cf7b4e82e23ab498837241
iBraskyy <72888867+iBraskyy@users.noreply.github.com>
2021-06-01 22:01:50 -0700

    added python file

---
+++ animals.py
@@ -0,0 +1,29 @@
+def L1():
+    name ="Panda"
+    type = "Land"
+    description = "rivan"
+    habitat = "50"
+    continent = "Asia"
+    folder ="barbarella" #name of folder file is stored in
+    info = {"name": name, "type": type, "description": description, "habitat": habitat, "continent": continent, "folder":folder}
+    return info
+
+def L2():
+    name ="Tiger"
+    type = "Land"
+    description = "rivan"
+    habitat = "50"
+    continent = "Asia"
+    folder ="barbarella" #name of folder file is stored in
+    info = {"name": name, "type": type, "description": description, "habitat": habitat, "continent": continent, "folder":folder}
+    return info
+
+def A1():
+    name ="Pteranadon"
+    type = "Air"
+    description = "rivan"
+    habitat = "50"
+    continent = "Asia"
+    folder ="barbarella" #name of folder file is stored in
+    info = {"name": name, "type": type, "description": description, "habitat": habitat, "continent": continent, "folder":folder}
+    return info
