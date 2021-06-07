68521dc28b9b233ef9c5715f149a859a3841b308
1843496 <72889902+1843496@users.noreply.github.com>
2021-06-07 11:19:03 -0700

    Create main.py

---
+++ potential blueprint/templates/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from second import second
+
+app = Flask(__name__)
+app.register_blueprint(second, url_prefix="/admin")
+# When running add /admin after url
+
+@app.route("/")
+def test():
+   return "<h1>Test1</h1>"
+
+
+if __name__ == "__main__":
+   app.run(debug=True)
